42256d4bfc2b59ccb7b6463beb3e57cf01aff12c
Ahmed Saad <ahmed.saad.8326@gmail.com>
2026-08-05 07:53:01 +0300

    Create initial project structure

---
+++ src/main/java/io/github/ahmedsaad167/bookstoremanager/app/Main.java
@@ -0,0 +1,8 @@
+package io.github.ahmedsaad167.bookstoremanager.app;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Bookstore Manager");
+    }
+}
