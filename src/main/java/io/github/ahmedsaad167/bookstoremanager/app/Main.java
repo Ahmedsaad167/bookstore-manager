0c0814839116230dd2ebbea98072395326ff5c46
Ahmed Saad <ahmed.saad.8326@gmail.com>
2026-08-05 22:37:39 +0300

    Create database schema

--- src/main/java/io/github/ahmedsaad167/bookstoremanager/app/Main.java
+++ src/main/java/io/github/ahmedsaad167/bookstoremanager/app/Main.java
@@ -1,18 +1,19 @@
 package io.github.ahmedsaad167.bookstoremanager.app;
 
+import io.github.ahmedsaad167.bookstoremanager.database.DatabaseInitializer;
 import io.github.ahmedsaad167.bookstoremanager.database.DatabaseManager;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 
 public class Main {
     
     public static void main(String[] args) {
-        try (Connection connection = DatabaseManager.getConnection()) {
-            System.out.println("Connected to SQLite successfully.");
+        try {           
+            DatabaseInitializer.initialize();
+            System.out.println("Database initializaed successfully.");
         } catch (SQLException e) {
-            System.out.println("Connection failed.");
+            System.out.println("Database initializaed failed.");
             e.printStackTrace();
         }
     }
---
+++ src/main/java/io/github/ahmedsaad167/bookstoremanager/database/DatabaseInitializer.java
@@ -0,0 +1,37 @@
+package io.github.ahmedsaad167.bookstoremanager.database;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class DatabaseInitializer {
+    public static void initialize() throws SQLException {
+        try (Connection connection = DatabaseManager.getConnection();
+            Statement statement = connection.createStatement()) {
+                createBooksTable(statement);
+            }                
+        }
+        
+        private static void createBooksTable(Statement statement) throws SQLException {
+            String sql = """
+                CREATE TABLE IF NOT EXISTS books (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    category TEXT NOT NULL,
+                    author TEXT NOT NULL,
+                    purchase_price REAL NOT NULL,
+                    selling_price REAL NOT NULL,
+                    stock_quantity INTEGER NOT NULL,
+                    material_type TEXT NOT NULL,
+                    publisher TEXT,
+                    publication_year INTEGER,
+                    isbn TEXT,
+                    age_group TEXT NOT NULL,
+                    notes TEXT
+                );
+            """;
+    
+            statement.executeUpdate(sql);
+        }
+}
+
