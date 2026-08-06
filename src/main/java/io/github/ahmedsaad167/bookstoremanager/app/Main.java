ca7572fe7ec948de520c34aeb8c4d28c0fe201ca
Ahmed Saad <ahmed.saad.8326@gmail.com>
2026-08-06 03:51:02 +0300

    Implement book save operation

--- src/main/java/io/github/ahmedsaad167/bookstoremanager/app/Main.java
+++ src/main/java/io/github/ahmedsaad167/bookstoremanager/app/Main.java
@@ -1,7 +1,9 @@
 package io.github.ahmedsaad167.bookstoremanager.app;
 
-import io.github.ahmedsaad167.bookstoremanager.database.DatabaseInitializer;
-import io.github.ahmedsaad167.bookstoremanager.database.DatabaseManager;
+import io.github.ahmedsaad167.bookstoremanager.dao.BookDao;
+import io.github.ahmedsaad167.bookstoremanager.model.AgeGroup;
+import io.github.ahmedsaad167.bookstoremanager.model.Book;
+import io.github.ahmedsaad167.bookstoremanager.model.MaterialType;
 
 import java.sql.SQLException;
 
@@ -9,12 +11,23 @@ import java.sql.SQLException;
 public class Main {
     
     public static void main(String[] args) {
-        try {           
-            DatabaseInitializer.initialize();
-            System.out.println("Database initializaed successfully.");
+        try {
+            Book cleanCode = new Book(
+                "Clean Code",
+                "Programming",
+                "Robert C. Martin",
+                250,
+                350,
+                10,
+                MaterialType.PAPER,
+                AgeGroup.ALL_AGES
+            );
+            BookDao dao = new BookDao();
+            int id = dao.save(cleanCode);
+            System.out.println("Book saved with id = " + id);
         } catch (SQLException e) {
-            System.out.println("Database initializaed failed.");
             e.printStackTrace();
         }
     }
 }
+    
