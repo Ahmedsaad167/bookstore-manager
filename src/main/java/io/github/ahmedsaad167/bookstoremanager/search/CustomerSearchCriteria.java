23b88c4f96408fca09305e22ff491bd7e5ce0900
Ahmed Saad <ahmed.saad.8326@gmail.com>
2026-08-11 10:39:46 +0300

    Add advanced search for customer

---
+++ src/main/java/io/github/ahmedsaad167/bookstoremanager/search/CustomerSearchCriteria.java
@@ -0,0 +1,49 @@
+package io.github.ahmedsaad167.bookstoremanager.search;
+
+public class CustomerSearchCriteria {
+    private String name;
+    private String username;
+    private String phone;
+    private String email;
+    private String address;
+    private String notes;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getNotes() {
+        return notes;
+    }
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    
+}
