75e9eefafe54494bc9cc737291e8f1b1bdcc304a
Ahmed Saad <ahmed.saad.8326@gmail.com>
2026-08-14 12:29:30 +0300

    Implement Customer Menu

--- src/main/java/io/github/ahmedsaad167/bookstoremanager/app/Main.java
+++ src/main/java/io/github/ahmedsaad167/bookstoremanager/app/Main.java
@@ -4,10 +4,13 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 import io.github.ahmedsaad167.bookstoremanager.dao.BookDao;
+import io.github.ahmedsaad167.bookstoremanager.dao.CustomerDao;
 import io.github.ahmedsaad167.bookstoremanager.dao.OrderDao;
 import io.github.ahmedsaad167.bookstoremanager.service.BookService;
+import io.github.ahmedsaad167.bookstoremanager.service.CustomerService;
 import io.github.ahmedsaad167.bookstoremanager.database.DatabaseInitializer;
 import io.github.ahmedsaad167.bookstoremanager.ui.BookMenu;
+import io.github.ahmedsaad167.bookstoremanager.ui.CustomerMenu;
 import io.github.ahmedsaad167.bookstoremanager.ui.MainMenu;
 
 public class Main {
@@ -20,11 +23,14 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
 
             BookDao bookDao = new BookDao();
+            CustomerDao customerDao = new CustomerDao();
             OrderDao orderDao = new OrderDao();
             BookService bookService = new BookService(bookDao, orderDao);
+            CustomerService customerService = new CustomerService(customerDao);
             BookMenu bookMenu = new BookMenu(scanner, bookService);
+            CustomerMenu customerMenu = new CustomerMenu(scanner, customerService);
             
-            MainMenu mainMenu = new MainMenu(scanner, bookMenu);
+            MainMenu mainMenu = new MainMenu(scanner, bookMenu, customerMenu);
 
             mainMenu.show();
 
